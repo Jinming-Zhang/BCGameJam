106b747eef3f896ed96c6f474ca697d3e162e79e
Szeka Tse <tse.s@northeastern.edu>
2022-09-17 14:43:32 -0700

    Player movement controller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,17 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof( CharacterController ))]
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private float playerSpeed = 1f;
+
+    private CharacterController characterController;
+
     void Start()
     {
-        
+        characterController = GetComponent<CharacterController>();
     }
-
-    // Update is called once per frame
+    
     void Update()
     {
+        // if (Input.GetKey(KeyCode.D))
+        // {
+        //     gameObject.transform.position += Vector3.right * speedPerSec * Time.deltaTime;
+        // }
+        // else if (Input.GetKey(KeyCode.A))
+        // {
+        //     gameObject.transform.position += Vector3.left * speedPerSec * Time.deltaTime;
+        // }
         
+        var move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+        characterController.Move(move * Time.deltaTime * playerSpeed);
     }
 }
