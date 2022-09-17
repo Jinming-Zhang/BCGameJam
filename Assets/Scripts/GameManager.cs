a715793c0d1bab716bbb13173213b6ebcbecec2a
Jinming Zhang <jinming.zhang47@gmail.com>
2022-09-17 19:31:25 -0400

    add end sliding background

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,6 +7,16 @@ public class GameManager : MonoBehaviour, IGameManager
 {
     private static GameManager instance;
     public static GameManager Instance => instance;
+
+
+    [Header("Game Ending settings")]
+    [SerializeField]
+    GameObject endingBackground;
+    [SerializeField]
+    Transform endingBackgroundDistination;
+    [SerializeField]
+    float slidingSpeed = 1f;
+
     private void Awake()
     {
         if (instance && instance != this)
@@ -37,9 +47,19 @@ public class GameManager : MonoBehaviour, IGameManager
         }
     }
 
+    [ContextMenu("End Restart")]
     public void EndGame()
     {
-        RestartGame();
+        StartCoroutine(MovingEndingInCR());
+        IEnumerator MovingEndingInCR()
+        {
+            Vector3 distinationPosition = endingBackgroundDistination.transform.position;
+            while (Vector3.Distance(endingBackground.transform.position, distinationPosition) > .001f)
+            {
+                endingBackground.transform.position = Vector3.MoveTowards(endingBackground.transform.position, distinationPosition, slidingSpeed * Time.deltaTime);
+                yield return new WaitForEndOfFrame();
+            }
+        }
     }
 
     [ContextMenu("Test Restart")]
