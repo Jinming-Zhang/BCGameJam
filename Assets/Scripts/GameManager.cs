8173fc7e789788e1d6e3c99671ad0b3de49cac9c
Jinming Zhang <jinming.zhang47@gmail.com>
2022-09-17 17:13:35 -0400

    making prefabs

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour, IGameManager
+{
+    public void EndGame()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void RestartGame()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void StartGame()
+    {
+        throw new System.NotImplementedException();
+    }
+}
--- Assets/Scripts/IGameManager.cs
+++ Assets/Scripts/IGameManager.cs
@@ -4,4 +4,7 @@ using UnityEngine;
 
 public interface IGameManager
 {
+    public void StartGame();
+    public void EndGame();
+    public void RestartGame();
 }
