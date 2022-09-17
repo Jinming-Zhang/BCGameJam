cc0a830f6fe7c72d9a6a06ae1925d47294c6b723
gerhardarmada31 <78196927+gerhardarmada31@users.noreply.github.com>
2022-09-17 15:12:05 -0700

    PowerUpBase
    The Base level of the PowerUp system

---
+++ Assets/Scripts/IPowerupable.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IPowerupable
+{
+    void SizeUp();
+    void SpeedUp();
+    void ForceUp();
+}
