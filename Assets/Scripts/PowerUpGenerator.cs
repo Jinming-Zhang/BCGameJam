fa4bc9334ea1782f00be45633f308d802247a8bf
Ziyun Chen <42946617+chenziy8@users.noreply.github.com>
2022-09-18 12:31:29 -0700

    Added assets into the proj
    add player, powerups sprites, fixed collision issue

--- Assets/Scripts/PowerUpGenerator.cs
+++ Assets/Scripts/PowerUpGenerator.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class PowerUpGenerator : MonoBehaviour
 {
-    [SerializeField] private GameObject powerUpObj;
-    [SerializeField] private int coolDownTimer;
+    [SerializeField] private List<GameObject> powerUpObj;
+    [SerializeField] private int coolDownTimer = 3;
+    public List<Sprite> sprites;
     //Const
     //private Vector3 xPosition = Camera.main.ViewportToWorldPoint(1,0.5,0);
 
@@ -13,16 +14,12 @@ public class PowerUpGenerator : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(CoolDown());
+        StartCoroutine(CoolDown());     
     }
 
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-       
-    }
+   
+ 
 
     IEnumerator CoolDown()
     {   float y = Random.Range(0, 1f);
@@ -34,6 +31,17 @@ public class PowerUpGenerator : MonoBehaviour
         {
             powerups.transform.position = spawnPos;
             powerups.transform.rotation = Quaternion.identity;
+            SpriteRenderer spriteImage = powerups.GetComponent(typeof(SpriteRenderer)) as SpriteRenderer;
+            int index = Random.Range(0, sprites.Count);
+            spriteImage.sprite = sprites[index];
+
+            PolygonCollider2D polyCollider = powerups.GetComponent(typeof(PolygonCollider2D)) as PolygonCollider2D;
+            Destroy(polyCollider);
+            PolygonCollider2D newPolyColliderpowerups = powerups.AddComponent(typeof(PolygonCollider2D)) as PolygonCollider2D;
+            newPolyColliderpowerups.isTrigger = true;
+           
+            
+            
             powerups.SetActive(true);
         }
         yield return new WaitForSeconds(coolDownTimer);
