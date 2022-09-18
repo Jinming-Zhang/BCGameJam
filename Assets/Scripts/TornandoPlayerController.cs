77f74b541f028c9f1c66cd852ff32b35bc071969
Szeka Tse <tse.s@northeastern.edu>
2022-09-17 17:08:05 -0700

    Clamp player position to screen

--- Assets/Scripts/TornandoPlayerController.cs
+++ Assets/Scripts/TornandoPlayerController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.UIElements;
 using UnityEngine;
 
 [RequireComponent(typeof( CharacterController ))]
@@ -22,12 +23,25 @@ public class TornandoPlayerController : PlayerController
     void Update()
     {
         var move = new Vector3(Input.GetAxis("Horizontal") * playerSpeedX, Input.GetAxis("Vertical") * playerSpeedY, 0);
+        var tryMove = gameObject.transform.position + move * Time.deltaTime;
+        // TODO: sprite width height
+        tryMove = Camera.main.WorldToViewportPoint(tryMove);
+        if (tryMove.x is > 1 or < 0 || tryMove.y is < 0 or > 1)
+        {
+            return;
+        }
         characterController.Move(move * Time.deltaTime);
     }
 
     public override void DoPowerup(float value)
     {
         PowerupCount += value;
+        if (PowerupCount <= 0) DoDie();
+    }
+
+    private void DoDie()
+    {
+        GameManager.Instance.EndGame();
     }
     
 }
