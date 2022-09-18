23bbc669ef9946e8b3321deb69e37e82aad2ad32
Szeka Tse <tse.s@northeastern.edu>
2022-09-18 10:48:36 -0700

    viewport min max default and temp disable

--- Assets/Scripts/TornandoPlayerController.cs
+++ Assets/Scripts/TornandoPlayerController.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 using UnityEngine.UIElements;
 
 [RequireComponent(typeof(CharacterController))]
-[RequireComponent(typeof(Collider))]
+// [RequireComponent(typeof(Collider))]
 public class TornandoPlayerController : PlayerController
 {
     [SerializeField] private float playerSpeedX = 1f;
@@ -13,6 +13,8 @@ public class TornandoPlayerController : PlayerController
     [SerializeField] private float initialPowerupCount = 1;
 
     private CharacterController characterController;
+    private Vector2 viewportMin;
+    private Vector2 viewportMax;
 
     public float PowerupCount { get; private set; }
 
@@ -21,28 +23,28 @@ public class TornandoPlayerController : PlayerController
     {
         characterController = GetComponent<CharacterController>();
         PowerupCount = initialPowerupCount;
+        viewportMin = UIManager.Instance.ViewportMin;
+        viewportMax = UIManager.Instance.ViewportMax;
     }
 
     void Update()
     {
         var move = new Vector3(Input.GetAxis("Horizontal") * playerSpeedX, Input.GetAxis("Vertical") * playerSpeedY, 0);
-        if (move.magnitude <= 0f) return;
-        // Debug.Log($"move: {move}");
+        // if (move.magnitude <= 0f) return;
 
-        var tryMoveMax = gameObject.transform.position +
-                         new Vector3(characterController.radius, characterController.height * .5f, 0) +
-                         move * Time.deltaTime;
-
-        var tryMoveMin = gameObject.transform.position -
-                         new Vector3(characterController.radius, characterController.height * .5f, 0) +
-                         move * Time.deltaTime;
-
+        var colliderOffset = new Vector3(characterController.radius, characterController.height * .5f, 0);
+        var tryMoveMax = gameObject.transform.position + colliderOffset + move * Time.deltaTime;
+        var tryMoveMin = gameObject.transform.position - colliderOffset + move * Time.deltaTime;
+        
+        
+        
         tryMoveMax = Camera.main.WorldToViewportPoint(tryMoveMax);
         tryMoveMin = Camera.main.WorldToViewportPoint(tryMoveMin);
         
         if (tryMoveMax.x > UIManager.Instance.ViewportMax.x || tryMoveMin.x < UIManager.Instance.ViewportMin.x) move.x = 0;
         if (tryMoveMax.y > UIManager.Instance.ViewportMax.y || tryMoveMin.y < UIManager.Instance.ViewportMin.y) move.y = 0;
-
+        // Debug.Log($"move: {move}, trymin:{tryMoveMin}, trymax:{tryMoveMax}, viewport min: {UIManager.Instance.ViewportMin}, viewport max:{UIManager.Instance.ViewportMax}");
+        
         characterController.Move(move * Time.deltaTime);
     }
 
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEditor.Animations;
 
 public class UIManager : MonoBehaviour
 {
@@ -16,13 +17,14 @@ public class UIManager : MonoBehaviour
 
     [SerializeField] TextMeshProUGUI countdownText;
 
-    public Vector2 ViewportMin { get; private set; }
-    public Vector2 ViewportMax { get; private set; }
+    public Vector2 ViewportMin { get; private set; } = Vector2.zero;
+    public Vector2 ViewportMax { get; private set; } = Vector2.one;
 
     private void setViewportMinMax()
     {
         var viewportCorners = new Vector3[4];
         gameArea.GetWorldCorners(viewportCorners); // It is screen corners, as RectTransform.position is screen space
+        Debug.Log(gameArea.localPosition);
 
         var minDist = 1f;
         var maxDist = 0f;
@@ -31,6 +33,7 @@ public class UIManager : MonoBehaviour
         for (var i = 0; i < 4; i++)
         {
             viewportCorners[i] *= -1f;
+            Debug.Log(viewportCorners[i]);
             viewportCorners[i] = Camera.main.ScreenToViewportPoint(viewportCorners[i]);
             viewportCorners[i].y = 1f - viewportCorners[i].y;
             var dist = Vector3.Distance(viewportCorners[i], Vector3.zero);
@@ -46,8 +49,27 @@ public class UIManager : MonoBehaviour
             }
         }
 
+        // var screenDimension = new Vector3(gameArea.rect.width, gameArea.rect.height, 0);
+        // vMin = gameArea.anchoredPosition3D - screenDimension * .5f - gameArea.localPosition;
+        // vMin = Camera.main.ScreenToViewportPoint(vMin);
+        // vMax = gameArea.anchoredPosition3D + screenDimension * .5f - gameArea.localPosition;
+        // vMax = Camera.main.ScreenToViewportPoint(vMax);
+        
+        // Vector2 size= Vector2.Scale(gameArea.rect.size, gameArea.lossyScale);
+        // float x= transform.position.x + gameArea.anchoredPosition.x;
+        // float y= Screen.height - gameArea.position.y - gameArea.anchoredPosition.y;
+        // var screenRect = new Rect(x, y, size.x, size.y);
+        
+        //  Vector2 size = Vector2.Scale(gameArea.rect.size, gameArea.lossyScale);
+        //  float x = gameArea.position.x - (gameArea.pivot.x * size.x);
+        //  float y = gameArea.position.y - ((1.0f - gameArea.pivot.y) * size.y);
+        //  var screenRect= new Rect(x, y, size.x, size.y);
+        // vMax =   -1 * Camera.main.ScreenToViewportPoint( screenRect.min );
+        // vMin = -1 *  Camera.main.ScreenToViewportPoint(screenRect.max);
+
         ViewportMin = vMin;
         ViewportMax = vMax;
+        Debug.Log($"min:{ViewportMin}, max:{ViewportMax}");
     }
 
     private void Awake()
@@ -65,7 +87,7 @@ public class UIManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        setViewportMinMax();
+        // setViewportMinMax();
     }
 
     // Update is called once per frame
