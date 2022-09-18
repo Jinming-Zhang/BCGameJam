30bdd0cb2b037a11a201c270a3957ddef2b5cdb3
kazumigames <tyip@kazumigames.com>
2022-09-18 10:51:18 -0700

    Version 1.2
    Main menu and transition menu

---
+++ Assets/Main Menu/Assets/Scenes/SecondMenu/LoadingMenu.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LoadingMenu : MonoBehaviour
+{
+    [SerializeField] Slider loadingSlider;
+    [SerializeField] float loadingTimer = 0.0f;
+    [SerializeField] string mainGame;
+    [SerializeField] GameObject gustSound;
+    private AudioSource audioSound;
+
+    [SerializeField] Sprite butterfly1;
+    [SerializeField] Sprite butterfly2;
+
+    [SerializeField] Button button;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSound = gustSound.GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TransitionToMainGame();
+        UpdateButterflyImage();
+    }
+
+    public void IncreaseLoadingTimer()
+    {
+        loadingTimer += 1;
+        loadingSlider.GetComponent<Slider>().value = loadingTimer;
+        Debug.Log(loadingSlider.GetComponent<Slider>().value);
+        playSound();
+    }
+
+    public void playSound()
+    {
+        audioSound.Play();
+    }
+
+    public void StopMenuMusic()
+    {
+        if (GameObject.FindGameObjectWithTag("BG Music") != null)
+        {
+            GameObject.FindGameObjectWithTag("BG Music").GetComponent<BGMusic>().StopMusic();
+        }
+    }
+
+    private void TransitionToMainGame()
+    {
+        if (loadingTimer >= 10)
+        {
+            StopMenuMusic();
+            Debug.Log("Load main game");
+            //SceneManager.LoadScene(mainGame);
+        }
+    }
+
+    private void UpdateButterflyImage()
+    {
+        float imageNo = loadingTimer % 2;
+        if(imageNo != 0)
+        {
+            button.GetComponent<Image>().sprite = butterfly2; //load image 2
+        }
+        else
+        {
+            //load image 1
+            button.GetComponent<Image>().sprite = butterfly1;
+        }
+    }
+}
